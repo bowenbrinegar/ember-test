ba1560760395f9ce79afe974db03d4d5c136f547
Bowen Brinegar <bowenbrinegar@gmail.com>
2018-05-07 21:30:22 -0400

    controller for form

--- app/controllers/tasks/new.js
+++ app/controllers/tasks/new.js
@@ -3,8 +3,10 @@ import Controller from '@ember/controller';
 export default Controller.extend({
     actions: {
         addTask: function() {
-            let title = this.get('task')
+            let title = this.get('title')
+            let holder = this.get('holder')
             alert(title)
+            alert(holder)
         }
     }
 });
